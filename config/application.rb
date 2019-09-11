fb501e09225fee80cfed4ab99a8ba513a6d05e6c
jake <skellytech.jg@gmail.com>
2019-09-11 04:25:35 +0000

    lesson 19 + precompile error resolved, page does not look updated, attempt to fix 1

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,6 @@ module Nomster
     # Precompile additional assets
     config.assets.precompile += %w( .svg .eot .woff .ttf )
     
-    config.assets.initialize_on_precompile = false
+    #config.assets.initialize_on_precompile = false
   end
 end
