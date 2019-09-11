af0ec0ecc21197d59955f574fea039522560fac5
jake <skellytech.jg@gmail.com>
2019-09-11 02:41:31 +0000

    lesson 17

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  #devise_for :users, path: 'auth', path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'cmon_let_me_in' }
     root 'places#index'
     resources :places
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
