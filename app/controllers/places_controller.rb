03bfe8a17224976d32b212aeed6dd21a8b3f4017
jake <skellytech.jg@gmail.com>
2019-09-12 12:57:49 +0000

    lesson 24

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -26,7 +26,13 @@ class PlacesController < ApplicationController
         @place = Place.find(params[:id])
         @place.update_attributes(place_params)
         redirect_to root_path
-    ends
+    end
+    
+    def destroy
+        @place = Place.find(params[:id])
+        @place.destroy
+        redirect_to root_path
+    end
     
     private
     
